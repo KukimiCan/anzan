974bd46d025463bd2829aa33908db5e26354be38
KukimiCan <148322666+KukimiCan@users.noreply.github.com>
2024-01-21 18:13:12 +0900

    Update main.js

--- AnZaN/play/main.js
+++ AnZaN/play/main.js
@@ -99,7 +99,7 @@ document.addEventListener('keyup', function(event) //キー入力を検知
                 input_len = 0;
                 player_ans = "_".repeat(ans_len);
                 false_num ++;
-                time += false_num*10000;
+                time += false_num*1000000000;
 
                 document.body.style.backgroundColor = "#f4b3c2";
 
